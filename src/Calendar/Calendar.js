0d14ef8347fca2e9f7718c4ef950e3a9d0b1c57b
Ticktock <smu@ticktock.local>
2018-05-17 10:16:41 +0800

    edited dynamic rendering of calendar

--- src/Calendar/Calendar.js
+++ src/Calendar/Calendar.js
@@ -16,12 +16,21 @@ class Calendar extends Component {
 	render(props){
 
 		const dayZero = Moment().format("ddd MMM D YYYY").toString();
+		console.log(dayZero);
 		const dayOne = Moment().subtract(1, 'days').format("ddd MMM D YYYY").toString();
 		const dayTwo = Moment().subtract(2, 'days').format("ddd MMM D YYYY").toString();
 		const dayThree = Moment().subtract(3, 'days').format("ddd MMM D YYYY").toString();
 		const dayFour = Moment().subtract(4, 'days').format("ddd MMM D YYYY").toString();
 		const dayFive = Moment().subtract(5, 'days').format("ddd MMM D YYYY").toString();
 		const daySix = Moment().subtract(6, 'days').format("ddd MMM D YYYY").toString();
+
+		let classZero;
+		let classOne;
+		let classTwo;
+		let classThree;
+		let classFour;
+		let classFive;
+		let classSix;
 
 		return(
 			<div className="tableContainer">
@@ -38,36 +47,73 @@ class Calendar extends Component {
 						<th className=" subzero today">{dayZero}</th>
 						<div className="arrow-up"></div>
 					</tr>
-						{ 
-							this.noteData.map((note, index) => {
-								let allSubject = [];
-								this.noteData.map((note) => {
-									allSubject.push(note.noteSubject);
-								});
-								let uniqueSubject = Array.from(new Set(allSubject));
-								console.log(uniqueSubject);
-								for (let i = 0; i < uniqueSubject.length; i++){
-									if(uniqueSubject[i]===note.noteSubject) {
-										return (
+						{ 		this.noteData.map((note) => {
+									let allSubject = [];
+									console.log(note);
+									this.noteData.map((note) => {
+										allSubject.push(note.noteSubject);
+									});
+									let uniqueSubject = Array.from(new Set(allSubject));
+									console.log(uniqueSubject);
+
+									for (let i = 0; i < uniqueSubject.length; i++){
+										if(uniqueSubject[i] === note.noteSubject && daySix === note.noteDate) {
+											classSix = "summary active"
+										} else {
+											classSix = "summary"
+										}
+										if(uniqueSubject[i] === note.noteSubject && dayFive === note.noteDate) {
+											classFive="summary active"
+										} else {
+											classSix = "summary"
+										}
+										if(uniqueSubject[i] === note.noteSubject && dayFour === note.noteDate) {
+											classFour="summary active"
+										} else {
+											classSix = "summary"
+										}
+										if(uniqueSubject[i] === note.noteSubject && dayThree === note.noteDate) {
+											classThree="summary active"
+										} else {
+											classSix = "summary"
+										}
+										if(uniqueSubject[i] === note.noteSubject && dayTwo === note.noteDate) {
+											classTwo="summary active"
+										} else {
+											classSix = "summary"
+										}
+										if(uniqueSubject[i] === note.noteSubject && dayOne === note.noteDate) {
+											classOne="summary active"
+										} else {
+											classSix = "summary"
+										}
+										if(uniqueSubject[i] === note.noteSubject && dayZero === note.noteDate) {
+											classZero="summary active"
+										} else {
+											classSix = "summary"
+										}
+									}
+
+									return (
 											<tr>
 											<td key={note.id} className="subject">{note.noteSubject}</td>
-											<td className="summary daySix">{note.noteDate === daySix ? 1 : 0}</td>
-											<td className="summary dayFive">{note.noteDate === dayFive ? 1 : 0}</td>
-											<td className="summary dayFour">{note.noteDate === dayFour ? 1 : 0}</td>
-											<td className="summary dayThree">{note.noteDate === dayThree ? 1 : 0}</td>
-											<td className="summary dayTwo">{note.noteDate === dayTwo ? 1 : 0}</td>
-											<td className="summary dayOne">{note.noteDate === dayOne ? 1 : 0}</td>
-											<td className="summary dayZero">{note.noteDate === dayZero ? 1 : 0}</td>
+											<td className={classSix}></td>
+											<td className={classFive}></td>
+											<td className={classFour}></td>
+											<td className={classThree}></td>
+											<td className={classTwo}></td>
+											<td className={classOne}></td>
+											<td className={classZero}></td>
 											</tr>)
+								
 									} 
-								}
-							})
-						}
-				</table>
-			</div>
+								)}
+							</table>
+						</div>
+					)
+				}
+			}
+		
 
-		)
-	}
-}
 
 export default Calendar;
